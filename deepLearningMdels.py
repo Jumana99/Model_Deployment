b47a727b79f5c151714dd49c1781c47d05df9e5b
Jumana Mohammad <73445192+Jumana99@users.noreply.github.com>
2022-05-27 22:52:20 +0300

    Add files via upload

--- deepLearningMdels.py
+++ deepLearningMdels.py
@@ -91,124 +91,125 @@ def clean_text(text):
     text = text.strip()
     return process_text(text)
 
-    def execute(tweet):
-        # Read the dataset and print it
-        data = pd.read_csv('ArSAS.csv')
-        # print(data.shape)
-        # print(data.dtypes)
-        # data.head()
 
-        data.drop(columns=['#Tweet_ID', 'Topic', 'Sentiment_label_confidence', 'Speech_act_label',
-                           'Speech_act_label_confidence'], inplace=True)
-        # print(data.shape)
-        # data.head()
+def execute(tweet):
+    # Read the dataset and print it
+    data = pd.read_csv('ArSAS.csv')
+    # print(data.shape)
+    # print(data.dtypes)
+    # data.head()
 
-        tweets = data['Tweet_text']
-        labels = data['Sentiment_label']
+    data.drop(columns=['#Tweet_ID', 'Topic', 'Sentiment_label_confidence', 'Speech_act_label',
+                       'Speech_act_label_confidence'], inplace=True)
+    # print(data.shape)
+    # data.head()
 
-        labels[data.Sentiment_label == 'Positive'] = 1
-        labels[data.Sentiment_label == 'Negative'] = -1
-        labels[data.Sentiment_label == 'Neutral'] = 2
+    tweets = data['Tweet_text']
+    labels = data['Sentiment_label']
 
-        labels_count = labels.value_counts()
-        labels_count.plot(kind="bar")
-        # print(labels.value_counts())
+    labels[data.Sentiment_label == 'Positive'] = 1
+    labels[data.Sentiment_label == 'Negative'] = -1
+    labels[data.Sentiment_label == 'Neutral'] = 2
 
-        # Only include postive, negative and neutral polarity:
-        data = data[data.Sentiment_label.isin([-1, 1, 2])]
+    labels_count = labels.value_counts()
+    labels_count.plot(kind="bar")
+    # print(labels.value_counts())
 
-        data['cleaned_text'] = tweets.apply(clean_text)
-        data = data[data.cleaned_text != ""]
-        # data.head(10)
+    # Only include postive, negative and neutral polarity:
+    data = data[data.Sentiment_label.isin([-1, 1, 2])]
 
-        # Balance input data (same size postive, negative, and neutral tweets):
+    data['cleaned_text'] = tweets.apply(clean_text)
+    data = data[data.cleaned_text != ""]
+    # data.head(10)
 
-        min_sample = data.groupby(['Sentiment_label']).count().Tweet_text.min()
-        input_data = pd.concat([data[data.Sentiment_label == 1].head(min_sample),
-                                data[data.Sentiment_label == -1].head(min_sample),
-                                data[data.Sentiment_label == 2].head(min_sample)
-                                ])
+    # Balance input data (same size postive, negative, and neutral tweets):
 
-        X = input_data.cleaned_text.values  # training paramter
-        Y = np.asarray(input_data.Sentiment_label.values).astype('float32')  # prediction paramter
-        Y = Y.clip(0, 2)
+    min_sample = data.groupby(['Sentiment_label']).count().Tweet_text.min()
+    input_data = pd.concat([data[data.Sentiment_label == 1].head(min_sample),
+                            data[data.Sentiment_label == -1].head(min_sample),
+                            data[data.Sentiment_label == 2].head(min_sample)
+                            ])
 
-        num_words = 10000
-        tokenizer = Tokenizer(num_words=num_words)
-        tokenizer.fit_on_texts(X)
+    X = input_data.cleaned_text.values  # training paramter
+    Y = np.asarray(input_data.Sentiment_label.values).astype('float32')  # prediction paramter
+    Y = Y.clip(0, 2)
 
-        X = tokenizer.texts_to_sequences(X)
+    num_words = 10000
+    tokenizer = Tokenizer(num_words=num_words)
+    tokenizer.fit_on_texts(X)
 
-        maxlen = 300
-        X = pad_sequences(X, padding='post', maxlen=maxlen)
-        total_words = len(tokenizer.word_index) + 1
+    X = tokenizer.texts_to_sequences(X)
 
-        seed = 42
-        X_train, X_test, label_train, label_test = train_test_split(X, Y, test_size=0.1, random_state=seed)
+    maxlen = 300
+    X = pad_sequences(X, padding='post', maxlen=maxlen)
+    total_words = len(tokenizer.word_index) + 1
 
-        NUM_WORDS = 10000
-        EMBEDDING_DIM = 300
-        vocabulary_size = 10000
+    seed = 42
+    X_train, X_test, label_train, label_test = train_test_split(X, Y, test_size=0.1, random_state=seed)
 
-        model = Sequential()
+    NUM_WORDS = 10000
+    EMBEDDING_DIM = 300
+    vocabulary_size = 10000
 
-        # embedding layer
-        model.add(Embedding(total_words, EMBEDDING_DIM, input_length=maxlen))
+    model = Sequential()
 
-        # convLayer1
-        model.add(layers.Conv1D(filters=32, kernel_size=4, padding='same', activation='relu'))
-        model.add(layers.MaxPooling1D(pool_size=2))
+    # embedding layer
+    model.add(Embedding(total_words, EMBEDDING_DIM, input_length=maxlen))
 
-        # convLayer2
-        model.add(layers.Conv1D(filters=64, kernel_size=5, padding='same', activation='relu'))
-        model.add(layers.MaxPooling1D(pool_size=4))
+    # convLayer1
+    model.add(layers.Conv1D(filters=32, kernel_size=4, padding='same', activation='relu'))
+    model.add(layers.MaxPooling1D(pool_size=2))
 
-        # convLayer3
-        model.add(layers.Conv1D(filters=128, kernel_size=6, padding='same', activation='relu'))
-        model.add(layers.MaxPooling1D(pool_size=8))
+    # convLayer2
+    model.add(layers.Conv1D(filters=64, kernel_size=5, padding='same', activation='relu'))
+    model.add(layers.MaxPooling1D(pool_size=4))
 
-        # lstm
-        model.add(layers.LSTM(64, dropout=0.2))
-        model.add(layers.Dense((128), activation='relu'))
-        model.add(layers.Dense((64), activation='relu'))
-        model.add(layers.Dense((3), activation='softmax'))
+    # convLayer3
+    model.add(layers.Conv1D(filters=128, kernel_size=6, padding='same', activation='relu'))
+    model.add(layers.MaxPooling1D(pool_size=8))
 
-        # dense
-        model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                      metrics=['accuracy'])
-        # model.summary()
+    # lstm
+    model.add(layers.LSTM(64, dropout=0.2))
+    model.add(layers.Dense((128), activation='relu'))
+    model.add(layers.Dense((64), activation='relu'))
+    model.add(layers.Dense((3), activation='softmax'))
 
-        # history = model.fit(X_train, label_train,
-        #                    epochs=30,
-        #                    verbose=True,
-        #                    validation_data=(X_test, label_test),
-        #                    batch_size=64)
+    # dense
+    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    # model.summary()
 
-        # loss, accuracy = model.evaluate(X_train, label_train, verbose=True)
-        # print("Training Accuracy: {:.4f}".format(accuracy))
+    # history = model.fit(X_train, label_train,
+    #                    epochs=30,
+    #                    verbose=True,
+    #                    validation_data=(X_test, label_test),
+    #                    batch_size=64)
 
-        # loss_val, accuracy_val = model.evaluate(X_test, label_test, verbose=True)
-        # print("Testing Accuracy:  {:.4f}".format(accuracy_val))
+    # loss, accuracy = model.evaluate(X_train, label_train, verbose=True)
+    # print("Training Accuracy: {:.4f}".format(accuracy))
 
-        # pickle.dump(model, open('model.pkl', 'wb'))
-        # pickled_model = pickle.load(open('model.pkl', 'rb'))
-        # model = load_model('cn_lstm')
-        model = pickle.load(open('model.pkl','rb'))
+    # loss_val, accuracy_val = model.evaluate(X_test, label_test, verbose=True)
+    # print("Testing Accuracy:  {:.4f}".format(accuracy_val))
 
-        sentiment = ['Negative', 'Positive', 'Neutral']
-        tweet = str(input('Enter Tweet: '))
-        tweets = clean_text(tweet)
+    # pickle.dump(model, open('model.pkl', 'wb'))
+    # pickled_model = pickle.load(open('model.pkl', 'rb'))
+    model = load_model('cn_lstm')
+    # model = pickle.load(open('model.pkl', 'rb'))
 
-        words = tweets.split(' ')
-        filtered = [w for w in words if w not in ar_stopwords]
-        filtered = ' '.join(filtered)
-        filtered = [filtered.lower()]
+    sentiment = ['Negative', 'Positive', 'Neutral']
+    # tweet = str(input('Enter Tweet: '))
+    tweets = clean_text(tweet)
 
-        # print('Filtered: ', filtered)
-        tokenize_words = tokenizer.texts_to_sequences(filtered)
-        tokenize_words = pad_sequences(tokenize_words, maxlen=300, padding='post', truncating='post')
-        # print(tokenize_words)
-        result = model.predict(tokenize_words)
-        # print(result)
-        # print(sentiment[np.around(result, decimals=0).argmax(axis=1)[0]])
-        return sentiment[np.around(result, decimals=0).argmax(axis=1)[0]]
+    words = tweets.split(' ')
+    filtered = [w for w in words if w not in ar_stopwords]
+    filtered = ' '.join(filtered)
+    filtered = [filtered.lower()]
+
+    # print('Filtered: ', filtered)
+    tokenize_words = tokenizer.texts_to_sequences(filtered)
+    tokenize_words = pad_sequences(tokenize_words, maxlen=300, padding='post', truncating='post')
+    # print(tokenize_words)
+    result = model.predict(tokenize_words)
+    # print(result)
+    # print(sentiment[np.around(result, decimals=0).argmax(axis=1)[0]])
+    return sentiment[np.around(result, decimals=0).argmax(axis=1)[0]]
--- server.py
+++ server.py
@@ -1,22 +1,15 @@
 from flask import Flask, request, jsonify
-import numpy as np
-import pickle
-import deepLearningMdels
+from deepLearningMdels import execute
 
 # model = pickle.load(open('model.pkl', 'rb'))
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return "Hello world"
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     text = request.form.get('text')
     # input_query = np.array([[text]])
-    result = deepLearningMdels.execute(text)
+    result = execute(text)
     return jsonify(result)
 
 
